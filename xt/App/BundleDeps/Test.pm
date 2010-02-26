6fa82d09d0a2637d5f7dd82f0962886f9c527151
lestrrat <lestrrat+github@gmail.com>
2010-02-26 15:21:42 +0900

    Add xt tests

---
+++ xt/App/BundleDeps/Test.pm
@@ -0,0 +1,30 @@
+package xt::App::BundleDeps::Test;
+use strict;
+use warnings;
+use base qw(Exporter);
+use App::BundleDeps;
+use File::Path;
+use File::Spec;
+
+our @EXPORT = qw(test_bundle);
+
+sub test_bundle ($) {
+    my $module = shift;
+    my $file = (caller())[1];
+    my $extlib = File::Spec->catfile($file . join("-", "", "extlib", time(), $$, rand() ) );
+    my $app = App::BundleDeps->new(
+        extlib => $extlib
+    );
+
+    $app->setup_deps( $module );
+    $app->bundle();
+
+    my $ok = 
+        ok( system($^X, "-Mlocal::lib=$extlib", "-M$module", '-e', "'print 1'") == 0,
+        "$module is installed in $extlib" );
+    if ($ok) {
+        rmtree($extlib);
+    }
+}
+
+1;
